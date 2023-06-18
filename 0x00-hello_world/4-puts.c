01c954daf80eb3b8baa857a4ab171ae9150b853f
Dernnik <oplumclothing07@gmail.com>
2023-06-18 09:25:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with put function
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+	return |(0);
+}
