de6e7592d56bea6179920395ee29d7e6199ad528
Dernnik <oplumclothing07@gmail.com>
2023-06-20 12:58:31 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/**
- * main - print _putchar
- * is written by dernnik
- * Return: 0
- */
 
+/**
+  * main - print _putchar
+  * is written by dernnik
+  * Return: 0
+  */
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
