d29f38460c7f5a044e4a8468288c847afa6fdac0
Dernnik <oplumclothing07@gmail.com>
2023-06-27 11:43:40 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,19 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _strlen - returns the length of a string
- * @: string
- * Return: length
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int longi = 0;
-		while (*s != '\0')
-		{
-			longi++;
-			s++;
-		}
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++
+	}
+
 	return (longi);
 }
