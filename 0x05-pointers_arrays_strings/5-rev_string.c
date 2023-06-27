e4fd07e5d768555844d0c97a29efc926433d16b9
Dernnik <oplumclothing07@gmail.com>
2023-06-27 15:37:57 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,20 +5,19 @@
  * @s: Input string
  * Return: String in reverse
  */
-void rev_string(char *s);
+void rev_string(char *s)
 {
 	char rev = s[0];
 	int counter = 0;
 	int i;
 
 	while (s[counter] != '\0')
-		counter++;
+	counter++;
 	for (i = 0; i < counter; i++)
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+	counter--;
+	rev = s[i];
+	s[i] = s[counter];
+	s[counter] = rev;
 	}
-	return (s);
 }
